ac386ce1bbad4560907dd983ae55da644d3b5e62
ChrisEng0212 <cjx02121981@gmail.com>
2020-02-20 23:44:23 +0800

    data_url

--- meta.py
+++ meta.py
@@ -3,14 +3,14 @@ import json
 import os
 
 try:
-    from aws import SECRETS
+    from aws import KEYS
     DEBUG = True
-    SCHEMA = SECRETS.SCHEMA
+    SCHEMA = KEYS.SCHEMA
 except: 
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID'] 
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY'] 
     SCHEMA = os.environ['SCHEMA']
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
     SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -20,12 +20,12 @@ jList = ['None', 'reading-lms', 'workplace-lms', 'abc-lms', 'peng-lms', 'food-lm
          
 
 s3_resource = boto3.resource('s3',
-        aws_access_key_id=SECRETS.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key= SECRETS.AWS_SECRET_ACCESS_KEY)
+        aws_access_key_id=KEYS.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key= KEYS.AWS_SECRET_ACCESS_KEY)
 
 s3_client = boto3.client('s3',
-         aws_access_key_id=SECRETS.AWS_ACCESS_KEY_ID,
-         aws_secret_access_key= SECRETS.AWS_SECRET_ACCESS_KEY)
+         aws_access_key_id=KEYS.AWS_ACCESS_KEY_ID,
+         aws_secret_access_key= KEYS.AWS_SECRET_ACCESS_KEY)
 
 
 def loadJson():
@@ -45,11 +45,11 @@ class BaseConfig:
     S3_BUCKET_NAME =  META['M']['S3_BUCKET_NAME']
     S3_LOCATION = 'https://' + S3_BUCKET_NAME + '.s3.ap-northeast-1.amazonaws.com/'
 
-    AWS_ACCESS_KEY_ID = SECRETS.AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY = SECRETS.AWS_SECRET_ACCESS_KEY
-    SQLALCHEMY_DATABASE_URI = SECRETS.SQLALCHEMY_DATABASE_URI  
-    MAIL_PASSWORD = SECRETS.MAIL_PASSWORD
-    SECRET_KEY = SECRETS.SECRET_KEY 
+    AWS_ACCESS_KEY_ID = KEYS.AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY = KEYS.AWS_SECRET_ACCESS_KEY
+    SQLALCHEMY_DATABASE_URI = KEYS.SQLALCHEMY_DATABASE_URI  
+    MAIL_PASSWORD = KEYS.MAIL_PASSWORD
+    SECRET_KEY = KEYS.SECRET_KEY 
 
     IDLIST = META['C']
 
