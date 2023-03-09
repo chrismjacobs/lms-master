3f11b1946a3b6d8fd4cf07706d9423a14fc7a3ac
ChrisEng0212 <cjx02121981@gmail.com>
2023-03-09 11:52:48 +0800

    publish fix up

--- static/js/writing/publish.js
+++ static/js/writing/publish.js
@@ -4,64 +4,82 @@ console.log(unit_number)
 
 let fullString = document.getElementById('fullDict').innerHTML
 let fullOBJ = JSON.parse(fullString)
-let info = JSON.parse(fullOBJ['info']) 
+let info = JSON.parse(fullOBJ['info'])
 
 
-let publish = JSON.parse(fullOBJ['publish']) 
+let publish = JSON.parse(fullOBJ['publish'])
 let imageLink = publish['imageLink']
 let title = publish['title']
 console.log(imageLink);
 
-
 // check revison has been done
-let revise = JSON.parse(fullOBJ['revise']) 
+let revise = JSON.parse(fullOBJ['revise'])
 
 let revised = revise['revised']
 if (revised == null){
     console.log('No data')
     alert('Please wait for instructors revision')
-    window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number   
+    window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number
 }
 
 let text = revise['text']
 
-startVue(info, revised)
+$.ajax({
+    type : 'POST',
+    url : '/getHTML/' + unit_number
+})
+.done(function(data) {
+        console.log(data);
 
-function startVue(info, revised){ 
-    let app = new Vue({   
+        let revise_obj = JSON.parse(data.revise)
+        let info = JSON.parse(data.info)
+        let html = revise_obj['html']
+        let text = revise_obj['text']
+        startVue(info, revised, html, text)
+});
+
+
+
+function startVue(info, revised, html, text){
+    let app = new Vue({
 
     el: '#vue-app',
     delimiters: ['[[', ']]'],
-    mounted : function (){ 
-        document.getElementById('final_image').src = imageLink   
-    },     
+    mounted : function (){
+        document.getElementById('final_image').src = imageLink
+    },
     data: {
-        publish : revised,       
-        info : info, 
-        base64data : null, 
-        fileType : null, 
-        title : title,        
+        publish : revised,
+        info : info,
+        base64data : null,
+        fileType : null,
+        title : title,
         save : false,
-        imageLink : imageLink,        
-        theme : { color : info['theme'],  display:'inline-block', 'font-size': '25px'}    
-    }, 
-    methods: { 
+        imageLink : imageLink,
+        theme : { color : info['theme'],  display:'inline-block', 'font-size': '25px'},
+        html : html,
+        text : text
+    },
+    methods: {
+        getImage: function (avatarImage) {
+            return 'https://writing-lms.s3.ap-northeast-1.amazonaws.com/profiles/avatar' + avatarImage + '.PNG'
+        },
         selectText: function(id){
-            console.log(id)            
-            document.getElementById(id).setAttribute('class', 'input2')          
+            console.log(id)
+            document.getElementById(id).setAttribute('class', 'input2')
         },
         deSelect: function(id, start){
             if (start!='start') {
                 this.save = true
-                }    
-            
-            console.log('SAVE', this.revText );            
-              
+                }
+
+            console.log('SAVE', this.revText );
+
             let textBox = document.getElementById(id)
             console.log('Height', textBox.scrollHeight)
-            textBox.setAttribute('class', 'input3') 
-            textBox.setAttribute('style', 'height:' + textBox.scrollHeight +'px !important')            
-        },  
+            textBox.setAttribute('class', 'input3')
+            textBox.setAttribute('style', 'height:' + textBox.scrollHeight +'px !important')
+        },
         image: function () {
             selectedFile = document.getElementById('pic').files[0]
 
@@ -71,98 +89,98 @@ function startVue(info, revised){
             form_data.append('image', document.getElementById('pic').files[0])
             form_data.append()
             console.log(form_data);
-            sendImage(form_data)       
-        }, 
-        fileValidation : function(){    
-          var fileInput = document.getElementById('pic');        
+            sendImage(form_data)
+        },
+        fileValidation : function(){
+          var fileInput = document.getElementById('pic');
           console.log('file', fileInput)
           var filePath = fileInput.value;
           console.log(filePath);
           app.fileType = filePath.split('.')[1]
 
           var allowedExtensions = /(\.jpeg|\.png|\.jpg)$/i;
-      
+
             if(fileInput.files[0].size > 4400000){
-                alert("File is too big!"); 
-                fileInput.value = '';             
-                return false;        
+                alert("File is too big!");
+                fileInput.value = '';
+                return false;
             }
             else if(!allowedExtensions.exec(filePath)){
                 alert('Please upload image: .jpeg/.png only.');
                 fileInput.value = '';
                 return false;
-            }    
-            else{            
-                console.dir( fileInput.files[0] );    
-                var url = window.URL.createObjectURL(fileInput.files[0]);        
+            }
+            else{
+                console.dir( fileInput.files[0] );
+                var url = window.URL.createObjectURL(fileInput.files[0]);
                 fetch(url)
                 .then(function(res){
                     return res.blob();
                     })
                 .then(function(savedBlob){
                     var reader = new FileReader();
-                    reader.readAsDataURL(savedBlob);          
+                    reader.readAsDataURL(savedBlob);
                     reader.onloadend = function() {
-                        app.base64data = reader.result.split(',')[1];  
-                        console.log(app.base64data); 
+                        app.base64data = reader.result.split(',')[1];
+                        console.log(app.base64data);
                         setTimeout(app.uploadImage() , 10000)
-                        
-                        } 
-                })  
+
+                        }
+                })
             }//end else
-              
-        },//end fileValidation  
+
+        },//end fileValidation
         uploadImage: function (){
             console.log()
-            $.ajax({    
+            $.ajax({
                 type : 'POST',
                 data : {
-                    unit : document.getElementById('unit').innerHTML, 
+                    unit : document.getElementById('unit').innerHTML,
                     b64String : app.base64data,
-                    fileType : app.fileType                                       
-                          
+                    fileType : app.fileType
+
                 },
-                url : '/sendImage',    
+                url : '/sendImage',
             })
-            .done(function(data) {              
+            .done(function(data) {
                 if (data) {
-                    console.log(data.imageLink);    
-                    app.imageLink = data.imageLink  
-                    document.getElementById('final_image').src = app.imageLink    
+                    console.log(data.imageLink);
+                    app.imageLink = data.imageLink
+                    document.getElementById('final_image').src = app.imageLink
                 }
             });
         },
-        sendData: function (){            
-            $.ajax({    
+        sendData: function (){
+            $.ajax({
                 type : 'POST',
                 data : {
-                    unit : document.getElementById('unit').innerHTML, 
+                    unit : document.getElementById('unit').innerHTML,
                     obj : null,
                     final : app.publish,
-                    title : app.title, 
+                    title : app.title,
                     imageLink : app.imageLink,
-                    stage : 5,                     
-                    work : 'publish'           
+                    stage : 5,
+                    work : 'publish'
                 },
-                url : '/storeData',    
+                url : '/storeData',
             })
-            .done(function(data) {              
-                if (data) {                
+            .done(function(data) {
+                if (data) {
                     alert('Thank you ' + data.name + ', your ' + data.work + ' has been saved')
-                    window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number         
+                    window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number
                 }
             });
         },
-        readRefs: function(){             
-            this.sendData(this.revText) 
-            alert('Please wait a moment while your writing is being updated') 
-        }, 
+        readRefs: function(){
+            this.sendData(this.revText)
+            alert('Please wait a moment while your writing is being updated')
+        },
         cancel: function(){
             alert('You have cancelled so your changes will not be saved')
-            window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number         
-        }      
+            window.location = (window.location.href).split('work')[0] + 'work/topic' + '/' + unit_number
+        }
     }
-    
+
 })// end NEW VUE
 
 }// end start vue function
